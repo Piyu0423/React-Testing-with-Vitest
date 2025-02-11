ec21a0adf530617df635e35de5d8675a28df5bd2
Piyu0423 <piumikamadubashini5@gmail.com>
2025-02-11 16:33:33 +0800

    unit test for login button

--- tests/components/greet.test.tsx
+++ tests/components/greet.test.tsx
@@ -10,4 +10,11 @@ describe("Greet", () => {
     expect(heading).toBeInTheDocument(); // first assertion to check theres a heading
     expect(heading).toHaveTextContent(/piumika/i); // second assertion to test the content
   });
+
+  it("should render login button when name is not provided", () => {
+    render(<Greet />);
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument(); // first assertion to check theres a button
+    expect(button).toHaveTextContent(/login/i); // second assertion to test the content
+  });
 });
