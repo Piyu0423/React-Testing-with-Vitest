04ee54f91e96a91f6e72692105acc4740ee20747
Piyu0423 <piumikamadubashini5@gmail.com>
2025-02-11 13:17:07 +0800

    test case for greet heading

---
+++ tests/components/greet.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import { it, expect, describe } from "vitest";
+import Greet from "../../src/components/Greet";
+import "@testing-library/jest-dom/vitest";
+
+describe("Greet", () => {
+  it("should render hello when name is present", () => {
+    render(<Greet name="Piumika" />);
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+  });
+});
