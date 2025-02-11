1020e550c97bbb93be175a0b8b318bd0d1d2abca
Piyu0423 <piumikamadubashini5@gmail.com>
2025-02-11 16:34:24 +0800

    adding vitest imports globally

--- tests/components/greet.test.tsx
+++ tests/components/greet.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import { it, expect, describe } from "vitest";
+// import { it, expect, describe } from "vitest";
 import Greet from "../../src/components/Greet";
 import "@testing-library/jest-dom/vitest";
 
