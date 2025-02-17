ca2afa1a2529e620ec086407d0fc4598857339d9
Piyu0423 <piumikamadubashini5@gmail.com>
2025-02-17 23:59:03 +0800

    Add UserEvent libarary to test user interaction with a checkbox

--- tests/components/TermsAndConditions.test.tsx
+++ tests/components/TermsAndConditions.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import TermsAndConditions from "../../src/components/TermsAndConditions";
+import userEvent from "@testing-library/user-event";
 
 describe("TermsAndConditions", () => {
   it("should render with correct text and initial text", () => {
@@ -16,5 +17,17 @@ describe("TermsAndConditions", () => {
     expect(button).toBeInTheDocument();
     expect(button).toHaveTextContent(/submit/i);
     expect(button).toBeDisabled();
+  });
+
+  it("should enabled the button is the checkbox is checked", async () => {
+    //Arrange
+    render(<TermsAndConditions />);
+    //Act
+    const checkbox = screen.getByRole("checkbox");
+    const user = userEvent.setup();
+    await user.click(checkbox);
+
+    //Assert
+    expect(screen.getByRole("button")).toBeEnabled();
   });
 });
